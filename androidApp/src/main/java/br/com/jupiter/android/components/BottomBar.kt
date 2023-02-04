f09e8b6e6ea83b11b2598dba729bb55ee59036ab
DanieleStein <danielestein@MacBook-Air-de-Daniele.local>
2023-02-04 14:32:17 -0300

    Alterado BottomBar

--- androidApp/src/main/java/br/com/jupiter/android/components/BottomBar.kt
+++ androidApp/src/main/java/br/com/jupiter/android/components/BottomBar.kt
@@ -1,9 +1,6 @@
 package br.com.jupiter.android.components
 
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.BottomAppBar
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
@@ -13,6 +10,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -22,8 +20,7 @@ import androidx.compose.ui.unit.sp
 fun BottomBar(title: String) {
   BottomAppBar(backgroundColor = Color(0xFF20B2AA)) {
     TextButton(onClick = { /*TODO*/ }) {
-      Text(text = title, fontSize = 20.sp, fontWeight = FontWeight.Bold, color = Color.White,  modifier = Modifier
-        .padding())
+      Text(text = title, fontSize = 20.sp, fontWeight = FontWeight.Bold, color = Color.White, textAlign = TextAlign.Right, modifier = Modifier.fillMaxWidth())
     }
   }
 }

ec3e1b148bfe6f4c4a028be5d20d81556b0fda1e
Daniele Stein <danieleastein@gmail.com>
2023-03-04 16:22:37 -0300

    Limpeza de código

--- androidApp/src/main/java/br/com/jupiter/android/courses/CourseScreen.kt
+++ androidApp/src/main/java/br/com/jupiter/android/courses/CourseScreen.kt
@@ -1,14 +1,12 @@
 package br.com.jupiter.android.courses
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Icon
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.Button
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Search
@@ -19,7 +17,6 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -48,7 +45,7 @@ fun CourseScreen(
     val listaCursos = remember(texto) { mutableStateOf(emptyList<Curso>()) }
 
 
-    MyApplicationTheme() {
+    MyApplicationTheme {
         Scaffold(
             topBar = {
                 TopBarPerfil(
